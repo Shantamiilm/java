7fe7015da9b4e249b44787030c86582031a775ea
Shantam <shantamforiilm@gmail.com>
2025-08-07 16:41:01 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter n1");
+        int n1 = sc.nextInt();
+        System.out.print("enter n2");
+        int n1 = sc.nextInt();
+        System.out.println(n1+"+"+  +"="+(n1+n2));
+    
+    }
+}
+
