667b7d086bb09cb22c00eaacaeec058322d2729e
Shantam <shantamforiilm@gmail.com>
2025-08-07 16:40:09 +0530

    Create address.java

---
+++ address.java
@@ -0,0 +1,10 @@
+public class address{
+    public static void main(String[]
+    args){
+    System.out.print(
+        "08,Bihar,India\n");
+        System.out.println("08");
+        System.out.println("Bihar");
+        System.out.println("India");
+    }
+}
